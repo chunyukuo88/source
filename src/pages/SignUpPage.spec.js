05068b0518a57aca5f424470c7e8b9dca206558d
chunyukuo88 <alexgochenour@gmail.com>
2021-09-10 14:40:39 -0400

    The issue was having to import jest dom from testing library.

--- src/pages/SignUpPage.spec.js
+++ src/pages/SignUpPage.spec.js
@@ -1,10 +1,10 @@
 import SignUpPage from "./SignUpPage.svelte";
 import { render, screen } from "@testing-library/svelte";
+import "@testing-library/jest-dom";
 
 it("displays a signup header.", () => {
   render(SignUpPage);
   const header = screen.getByRole("heading", { name: "Sign Up" });
 
-  //   expect(header).toBeInTheDocument();
-  expect(header).toBeTruthy();
+  expect(header).toBeInTheDocument();
 });
