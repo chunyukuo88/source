cd1d9b4fd65837141ad9fe42b9cf809746626fbf
Alex Gochenour <alexgochenour@gmail.com>
2021-09-10 22:18:47 -0400

    WIP switching comps

--- src/pages/SignUpPage.spec.js
+++ src/pages/SignUpPage.spec.js
@@ -2,9 +2,38 @@ import SignUpPage from "./SignUpPage.svelte";
 import { render, screen } from "@testing-library/svelte";
 import "@testing-library/jest-dom";
 
-it("displays a signup header.", () => {
-  render(SignUpPage);
-  const header = screen.getByRole("heading", { name: "Sign Up" });
+describe("SignUpPage.svelte", ()=>{
+  describe("Layout rendering", ()=>{
+    it("displays a signup header.", () => {
+      render(SignUpPage);
+      const header = screen.getByRole("heading", { name: "sign up" });
 
-  expect(header).toBeInTheDocument();
+      expect(header).toBeInTheDocument();
+    });
+    it("has username input.", () => {
+      render(SignUpPage);
+      const input = screen.getByLabelText("username");
+
+      expect(input).toBeInTheDocument();
+    });
+    it("has email input.", () => {
+      render(SignUpPage);
+      const input = screen.getByLabelText("email");
+
+      expect(input).toBeInTheDocument();
+    });
+    it("has a password input.", () => {
+      render(SignUpPage);
+      const input = screen.getByLabelText("password");
+
+      expect(input).toBeInTheDocument();
+    });
+  });
+  describe("Other functionality", ()=>{
+    it('The password input is of "password" type', ()=>{
+      render(SignUpPage);
+      const input = screen.getByLabelText('password');
+      expect(input.type).toBe('password');
+    });
+  });
 });
--- src/pages/SignUpPage.svelte
+++ src/pages/SignUpPage.svelte
@@ -1,1 +1,10 @@
-<h1>Sign Up</h1>
+<h1>sign up</h1>
+
+<label for="username">username</label>
+<input id="username"/>
+
+<label for="email">email</label>
+<input id="email"/>
+
+<label for="password">password</label>
+<input type="password" id="password"/>
